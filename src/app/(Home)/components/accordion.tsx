ce63fd4b8ae989599b894157fd707b1c9a6d8745
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-12-20 20:01:27 -0300

    fix : styled add new

--- src/app/(Home)/components/accordion.tsx
+++ src/app/(Home)/components/accordion.tsx
@@ -8,26 +8,32 @@ import {
 export function AccordionComponent() {
   const accordionsItens = [
     {
-      Trigger: 'Pergunta numero 01',
-      content: 'Conteúdo da pergunta ',
+      Trigger: 'Como você aborda a responsividade em seus projetos front-end ?',
+      content:
+        'Na abordagem da responsividade, priorizo o design fluido e a utilização de media queries para garantir uma experiência consistente em diversos dispositivos. Testo amplamente em dispositivos móveis e desktops, assegurando que o layout se adapte de forma eficaz a diferentes tamanhos de tela.',
       id: '1',
       value: 'item-01',
     },
     {
-      Trigger: 'Pergunta numero 02',
-      content: 'Conteúdo da pergunta',
+      Trigger: 'Como você incorpora práticas de SEO em seus projetos ?',
+      content:
+        'Integro práticas de SEO desde o início do desenvolvimento, garantindo o uso adequado de tags HTML, meta tags descritivas e URLs amigáveis. Utilizo semantic HTML para destacar a estrutura da página aos motores de busca e otimizo o conteúdo multimídia para melhorar a indexação.',
       id: '2',
       value: 'item-02',
     },
     {
-      Trigger: 'Pergunta numero 03',
-      content: 'Conteúdo da pergunta',
+      Trigger:
+        ' Qual é a sua abordagem para garantir a performance em aplicações web ?',
+      content:
+        'Para garantir a performance, otimizo o carregamento de recursos assíncronos, priorizo o carregamento de conteúdo visível e utilizo técnicas como code splitting. Além disso, realizo testes de desempenho com ferramentas como Lighthouse para identificar áreas de melhoria. O monitoramento contínuo do desempenho da aplicação permite ajustes rápidos e eficazes para garantir uma experiência ágil aos usuários.',
       id: '3',
       value: 'item-03',
     },
     {
-      Trigger: 'Pergunta numero 04',
-      content: 'Conteúdo da pergunta',
+      Trigger:
+        'Como você garante a segurança nas aplicações front-end que desenvolve ?',
+      content:
+        'A segurança é uma prioridade em meus projetos. Utilizo HTTPS para garantir a transferência segura de dados e evito o uso de bibliotecas desatualizadas ou com vulnerabilidades conhecidas. Implemento práticas de segurança no código, como validação de entrada, sanitização de dados e proteção contra ataques XSS (Cross-Site Scripting) e CSRF (Cross-Site Request Forgery).',
       id: '4',
       value: 'item-04',
     },
@@ -38,10 +44,10 @@ export function AccordionComponent() {
       <Accordion className="w-full" type="single" collapsible>
         {accordionsItens.map((item) => (
           <AccordionItem key={item.id} value={item.value}>
-            <AccordionTrigger className="bg-zinc-800 text-zinc-50 font-semibold text-xl p-2 h-16 rounded-sm">
+            <AccordionTrigger className="bg-zinc-800 text-zinc-50 font-semibold text-xl text-start p-2 h-20 rounded-sm">
               {item.Trigger}
             </AccordionTrigger>
-            <AccordionContent className="bg-zinc-900 text-zinc-50 text-md p-2 h-20">
+            <AccordionContent className="bg-zinc-900 text-zinc-50 text-md p-2">
               {item.content}
             </AccordionContent>
           </AccordionItem>
--- src/app/(Home)/components/sectionFAQs.tsx
+++ src/app/(Home)/components/sectionFAQs.tsx
@@ -1,22 +1,20 @@
 import Link from 'next/link'
 import { AccordionComponent } from './accordion'
+import Button from '@/components/Buttons/Button'
 
 export function SectionFAQs() {
   return (
     <section className="py-28 flex flex-col lg:flex-row items-start gap-20">
-      <div className="w-full flex items-start justify-between gap-20 p-10">
+      <div className="w-full flex flex-col lg:flex-row items-start justify-between gap-20 lg:p-10">
         <div className="flex flex-col items-start justify-center gap-2  ">
-          <h5 className="text-zinc-50 font-bold text-5xl">FAQS</h5>
-          <p className="text-zinc-100 font-normal text-lg">
+          <h5 className="text-zinc-50 font-bold text-7xl">FAQS</h5>
+          <p className="text-zinc-100 font-normal text-2xl">
             Dúvidas? Consulte Nosso FAQ: Seu Mapa para Soluções Rápidas e
             Descomplicadas.
           </p>
-          <Link
-            className="border border-zinc-50 text-zinc-100 p-1 rounded-sm"
-            href={''}
-          >
-            Contato
-          </Link>
+          <Button className="w-24" variant="highlight">
+            <Link href={'/Contact'}>Contato</Link>
+          </Button>
         </div>
         <div className="w-full ">
           <AccordionComponent />
