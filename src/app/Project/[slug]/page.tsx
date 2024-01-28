d8851b28e7dbadd198f47b936be641c90ede4034
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-01-28 11:09:31 -0300

    styles : add new styles and components

--- src/app/Project/[slug]/page.tsx
+++ src/app/Project/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function ProjectPageDetails({
     <div className="flex flex-col items-center justify-center py-28  gap-20">
       <div className="w-full lg:px-16">
         <Image
-          className="w-full h-[450px] "
+          className="w-full h-[450px] rounded-lg "
           alt={projectDetail.title}
           src={projectDetail.coverImage.url}
           width={500}
@@ -36,19 +36,19 @@ export default async function ProjectPageDetails({
           quality={100}
         />
       </div>
-      <div className="lg:w-[768px] w-full lg:py-20 lg:px-16 py-10  space-y-16 ">
+      <div className="lg:w-[768px] w-full lg:py-20  py-10  space-y-16 ">
         <div className="flex flex-col gap-6 ">
           <h2 className="text-zinc-50 font-bold text-6xl">
             {projectDetail.title}
           </h2>
-          <p className="text-zinc-300 font-normal text-md">
+          <p className="text-zinc-300 font-normal text-sm">
             {projectDetail.description}
           </p>
           <div className="flex flex-col gap-3">
             <span className="font-normal text-xl text-violet-700">
               Tecnologias ulitiladas
             </span>
-            <div className="flex items-center justify-start gap-2">
+            <div className="flex items-center justify-start flex-wrap gap-2">
               {tech?.map((i) => (
                 <span
                   className="bg-zinc-950 text-zinc-50 p-2 rounded-md shadow-md"
